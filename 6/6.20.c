cf40412003d9077695c2a29efa504ba59ca9216a
mengxin <2478444358@qq.com>
2020-08-15 13:36:39 +0800

    2020 8 15

---
+++ 6/6.20.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+double power(double n, int p);
+int main (void)
+{
+    double x, xpow;
+    int exp;
+
+    printf("Enter a number and the positive integer power");
+    printf(" to which\nthe number will be raised. Enter q");    
+    printf(" to quit .\n");
+    while (scanf("%1f%d", &x, &exp) == 2)
+    {
+
+        
+    }    
+}
