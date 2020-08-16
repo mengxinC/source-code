7cc69f9fc1ef557ff7e09766aa516d6219ab9152
your name <your email address>
2020-08-16 23:06:06 +0800

    2

--- 6/6.20.c
+++ 6/6.20.c
@@ -10,7 +10,23 @@ int main (void)
     printf(" to quit .\n");
     while (scanf("%1f%d", &x, &exp) == 2)
     {
-// CNB
-        
-    }    
+        xpow = power(x, exp);
+        printf("%.3g to the power %d is %.5g\n", x, exp, xpow);
+        printf("Enter next pair of numbera or q to quit.\n");
+    }
+    printf("Hope you enjoyed this power trip -- bye!\n");
+
+    getchar();
+    getchar();
+}
+
+double power (double n, int p)
+{
+    double pow = 1;
+    int i;
+
+    for(i = 1; i <= p; i++)
+        pow *= n;
+
+        return pow;
 }
