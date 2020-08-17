1f7e942c46c7dc3cbae35bb2f41364e9737f5ca2
your name <your email address>
2020-08-17 15:37:46 +0800

    1

---
+++ 6/6.16.18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (void)
+{
+   int  size, number, n;
+
+   while (number < 151)
+   {
+       while()
+       {
+       number = 2 * (number - size );
+       ++size;
+       }
+   }
+}
