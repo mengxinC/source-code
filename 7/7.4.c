59eb1d772708f5957fd7b036817e3522957aa40b
your name <your email address>
2020-08-18 20:16:41 +0800

    7

--- 7/7.4.c
+++ 7/7.4.c
@@ -22,7 +22,7 @@ int main (void)
         bill = BASE1 + (RATE2 * (kwh - BREAK1));
     else if (kwh <= BREAK3)
         bill = BASE2 + (RATE3 * (kwh - BREAK2));
-    else
+    else 
         bill = BASE3 + (RATE4 * (kwh - BREAK3));
     printf("The charge for %.1f kwh is $%1.2f.\n", kwh, bill);
 
